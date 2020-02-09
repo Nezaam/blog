ac0fabb13a25a17976784c6bf2d3470fc052127a
Nezaam <ilgiz.krymov@gmail.com>
2020-02-09 11:09:02 +0500

    Add repository for comments and posts

---
+++ src/main/scala/com/nezaam/blog/routes/Comments.scala
@@ -0,0 +1,18 @@
+package com.nezaam.blog.routes
+
+import cats.Applicative
+import cats.implicits._
+import com.nezaam.blog.models.{ Comment, Post }
+import org.joda.time.DateTime
+
+trait Comments[F[_]] {
+  def get: F[Comment]
+}
+
+object Comments {
+  def apply[F[_]](implicit ev: Comments[F]): Comments[F] = ev
+
+  def impl[F[_]: Applicative]: Comments[F] = new Comments[F] {
+    override def get: F[Comment] = Comment(1, DateTime.now(), DateTime.now(), "", "", Post(0, "Some title", "Nezaam", DateTime.now(), None, "Some text", List(), List())).pure[F]
+  }
+}
---
+++ src/main/scala/com/nezaam/blog/routes/Posts.scala
@@ -0,0 +1,18 @@
+package com.nezaam.blog.routes
+
+import cats.Applicative
+import cats.implicits._
+import com.nezaam.blog.models.Post
+import org.joda.time.DateTime
+
+trait Posts[F[_]] {
+  def get: F[Post]
+}
+
+object Posts {
+  def apply[F[_]](implicit ev: Posts[F]): Posts[F] = ev
+
+  def impl[F[_]: Applicative]: Posts[F] = new Posts[F] {
+    def get: F[Post] = Post(0, "Some title", "Nezaam", DateTime.now(), None, "Some text", List(), List()).pure[F]
+  }
+}
