8892530f3174c698835d464ac32493cd924e6088
Nezaam <ilgiz.krymov@gmail.com>
2020-02-09 11:11:11 +0500

    Fix routes and server

--- src/main/scala/com/nezaam/blog/BlogRoutes.scala
+++ src/main/scala/com/nezaam/blog/BlogRoutes.scala
@@ -2,19 +2,32 @@ package com.nezaam.blog
 
 import cats.effect.Sync
 import cats.implicits._
+import com.nezaam.blog.routes.{ Comments, Posts }
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 
 object BlogRoutes {
 
-  def topicRoutes[F[_]: Sync](T: Topics[F]): HttpRoutes[F] = {
+  def postRoutes[F[_]: Sync](T: Posts[F]): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F] {}
     import dsl._
     HttpRoutes.of[F] {
-      case GET -> Root / "topic" ⇒
+      case GET -> Root / "post" ⇒
         for {
           topic ← T.get
           resp ← Ok(topic)
+        } yield resp
+    }
+  }
+
+  def commentsRoutes[F[_]: Sync](T: Comments[F]): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F] {}
+    import dsl._
+    HttpRoutes.of[F] {
+      case GET -> Root / "comments" ⇒
+        for {
+          comment ← T.get
+          resp ← Ok(comment)
         } yield resp
     }
   }
--- src/main/scala/com/nezaam/blog/BlogServer.scala
+++ src/main/scala/com/nezaam/blog/BlogServer.scala
@@ -1,6 +1,9 @@
 package com.nezaam.blog
 
+import cats.implicits._
 import cats.effect.{ ConcurrentEffect, ContextShift, Timer }
+import com.nezaam.blog
+import com.nezaam.blog.routes.{ Comments, Posts }
 import fs2.Stream
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
@@ -9,9 +12,13 @@ import org.http4s.server.middleware.Logger
 object BlogServer {
 
   def stream[F[_]: ConcurrentEffect](implicit T: Timer[F], C: ContextShift[F]): Stream[F, Nothing] = {
-    val topicAlg = Topics.impl[F]
+    val postAlg = Posts.impl[F]
+    val commentAlg = Comments.impl[F]
 
-    val httpApp = BlogRoutes.topicRoutes[F](topicAlg).orNotFound
+    val httpApp = (
+      BlogRoutes.postRoutes[F](postAlg) <+>
+      BlogRoutes.commentsRoutes[F](commentAlg)
+    ).orNotFound
 
     val finalHttpApp = Logger.httpApp(logHeaders = true, logBody = true)(httpApp)
     for {
